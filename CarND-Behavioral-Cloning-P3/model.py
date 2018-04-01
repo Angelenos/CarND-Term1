3272dceee161e15cdbb654b5037983d5d8ff003e
Enos <enos@umich.edu>
2018-03-31 20:50:17 -0400

    Adding dropout layers

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -18,6 +18,8 @@ from keras.layers import Flatten, Dense, Cropping2D, Lambda, Convolution2D, Drop
 # Data corresponding to different driving strategies are stored in different folders under \data\
 # Search all subfolders to acquire complete data
 lines = []
+images = []
+angles = []
 for folder in os.listdir('./data'):
     csv_folder = 'data/' + folder
     if os.path.exists(csv_folder + '/driving_log.csv'):
@@ -25,34 +27,18 @@ for folder in os.listdir('./data'):
             reader = csv.reader(csvfile)
             for line in reader:
                 lines.append(line)
-
-
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
-
-
-# Definition of Generator functions
-def generator(samples, batch_size=64):
-    num_samples = len(samples)
-    while 1:  # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name_center = batch_sample[0].split('/')[-1]
-                name_left = batch_sample[1].split('/')[-1]
+                name_center = line[0].split('/')[-1]
+                name_left = line[1].split('/')[-1]
                 center_image = cv2.imread(name_center)
                 if center_image is not None:
                     # Adopt images from left camera to compensate a constant negative steering angles observed when
                     # vehicle is driving on the straight line
                     left_image = cv2.imread(name_left)
-                    center_angle = float(batch_sample[3])
+                    center_angle = float(line[3])
                     if center_angle > 0:
                         left_angle = center_angle
                     else:  # Apply negative offsets, which scaled with the actual steering angles, to the original value
-                        left_angle = center_angle + (0.05 * (1.5 + center_angle))
+                        left_angle = center_angle + (0.05 * (1.8 + center_angle))
                     image_flipped = np.fliplr(center_image)
                     angle_flipped = -center_angle
                     images.append(center_image)
@@ -62,19 +48,37 @@ def generator(samples, batch_size=64):
                     images.append(left_image)
                     angles.append(left_angle)
 
+# Split training and validation sample after manipulating the original ones to expand the working range
+train_samples_x, validation_samples_x, train_samples_y, validation_samples_y = train_test_split(images, angles,
+                                                                                                test_size=0.2)
+train_size = len(train_samples_y)
+valid_size = len(validation_samples_y)
+print('Training set size: {}'.format(train_size))
+print('Validation set size: {}'.format(valid_size))
+
+
+# Definition of Generator functions
+def generator(x, y, batch_size=64):
+    num_samples = len(y)
+    while 1:  # Loop forever so the generator never terminates
+        # shuffle(x, y)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples_x = x[offset:offset + batch_size]
+            batch_samples_y = y[offset:offset + batch_size]
             # trim image to only see section with road
-            x_train = np.array(images)
-            y_train = np.array(angles)
+            x_train = np.array(batch_samples_x)
+            y_train = np.array(batch_samples_y)
             yield shuffle(x_train, y_train)
 
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=64)
-validation_generator = generator(validation_samples, batch_size=64)
+train_generator = generator(train_samples_x, train_samples_y, batch_size=128)
+validation_generator = generator(validation_samples_x, validation_samples_y, batch_size=128)
 
 # Definition of Hyper Parameters
 epochs = 4
 ch, row, col = 3, 65, 320  # Trimmed image format
+drop_rate = 0.2
 
 # Layout of model
 model = Sequential()
@@ -87,20 +91,23 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(drop_rate))
 model.add(Dense(50))
+model.add(Dropout(drop_rate))
 model.add(Dense(10))
+model.add(Dropout(drop_rate))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
+history_object = model.fit_generator(train_generator, samples_per_epoch=train_size,
                                      validation_data=validation_generator,
-                                     nb_val_samples=len(validation_samples), nb_epoch=epochs)
+                                     nb_val_samples=valid_size, nb_epoch=epochs)
 
 # Save Model
 model.save('model.h5')
 
 # Plot loss versus EPOCHS
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
+plt.plot([i for i in range(1, epochs + 1)], history_object.history['loss'])
+plt.plot([i for i in range(1, epochs + 1)], history_object.history['val_loss'])
 plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
--- CarND-Behavioral-Cloning-P3/writeup.md
+++ CarND-Behavioral-Cloning-P3/writeup.md
@@ -55,13 +55,15 @@ The model adopted in this project is published by Nvidia. Here is a visualizatio
 
 #### 2. Attempts to reduce overfitting in the model
 
-The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 29). 20% of the training set was randomly chosen to be the validation set.
+The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 29). 20% of the training set was randomly chosen to be the validation set after manupulations over the data including flipping images horizontally and adding images from left cameras.
+
+Dropout layers are introduced into the model with dropout rate of 0.2 to combat overfitting.
 
 The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
 
 #### 3. Model parameter tuning
 
-The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 92).
+The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 100).
 
 #### 4. Appropriate training data
 
@@ -84,7 +86,7 @@ My first step was to use a convolution neural network model similar to the Nvidi
 
 In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting.
 
-To combat the overfitting, I modified the epochs from the initial value of 5 to 3, and the validation loss is now dropping monotonously. I also planned to introduce dropout layer between fully connected ones to further reduce overfitting. However the model started to ignore some training data I include such as sharp steering and maintaining vehicle orientations when entering/exiting bridges. This indicated that with a small size of training data it is not beneficial to introduce dropout layer. Due to the some reason I reduce the size of validation set to be 20% of the entire training data to reduce the risk of not picking any of the cases I include in the training set.
+To combat the overfitting, I introduced dropout layers between the fully connected ones with a dropout rate of 0.2, and extended the epochs to 4.
 
 The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track:
 
@@ -99,7 +101,7 @@ At the end of the process, the vehicle is able to drive autonomously around the 
 
 #### 2. Final Model Architecture
 
-The final model architecture shares the same acchitecture with the Nvidia self-driving car model excepting adding a 2D cropping layers to remove useless information including background and sky from the training data. The architecture consists of (model.py lines 79-95):
+The final model architecture shares the same acchitecture with the Nvidia self-driving car model excepting adding a 2D cropping layers to remove useless information including background and sky from the training data. The architecture consists of (model.py lines 84-99):
 
 1. Lambda layer which normalizes the original image into values between -0.5 to 0.5
 2. A 2D Cropping layer which crops the upper 70 and lower 25 pixels of the image
@@ -110,9 +112,12 @@ The final model architecture shares the same acchitecture with the Nvidia self-d
 7. 2D Conv layer with 64 filters of 3x3 and "ReLu" activation function
 8. Flatten layer
 9. Fully connected layer leads to 100 neurons
-10. Fully connected layer leads to 50 neurons
-11. Fully connected layer leads to 10 neurons
-12. Fully connected layer leads to 1 neurons
+10. Dropout layer with dropout rate of 0.2
+11. Fully connected layer leads to 50 neurons
+12. Dropout layer with dropout rate of 0.2
+13. Fully connected layer leads to 10 neurons
+14. Dropout layer with dropout rate of 0.2
+15. Fully connected layer leads to 1 neurons
 
 #### 3. Creation of the Training Set & Training Process
 
@@ -134,7 +139,7 @@ Also I include situations when the road shoulder has marks of white-red strips, 
 
 To augment the data sat, I also flipped images and angles thinking that this would provide training data for right steering, which is much less than the left steering on the test track.
 
-After the collection process, I had 10683 numbers of data points. I then preprocessed this data by normalizing the R, G and B channels with 255 and then centered to 0. Also since it is not necessary to include background such as mountains, skies, etc., all images are cropped to remove the top 70 and bottom 25 pixels. This also reduce the computation time to train the model
+After the collection process, I had 17102 numbers of data points. I then preprocessed this data by normalizing the R, G and B channels with 255 and then centered to 0. Also since it is not necessary to include background such as mountains, skies, etc., all images are cropped to remove the top 70 and bottom 25 pixels. This also reduce the computation time to train the model
 
 I finally randomly shuffled the data set and put 20% of the data into a validation set.
 
