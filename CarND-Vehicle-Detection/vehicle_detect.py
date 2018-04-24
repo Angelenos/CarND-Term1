fcdf7f7d8c2a50f3feef8b386269a72d3ed06cf5
Enos <enos@umich.edu>
2018-04-24 08:54:25 -0400

    First version run with project video

--- CarND-Vehicle-Detection/vehicle_detect.py
+++ CarND-Vehicle-Detection/vehicle_detect.py
@@ -4,7 +4,7 @@
 ################################################################################################
 # This is the implementation of vehicle detection pipeline
 
-# Program tested on personal laptop with Intel 6820HK CPU + Nvidia GTX980 Graphic Card
+# Program tested on personal laptop with Intel 6820HK CPU + 32GB RAM + Nvidia GTX980 Graphic Card
 
 import sys
 import os
@@ -15,6 +15,7 @@ from moviepy.editor import VideoFileClip
 import sklearn.utils
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 
 from lesson_functions import *
@@ -23,39 +24,43 @@ from lesson_functions import *
 # Definition of Hyper parameters
 Ystart = 400
 Ystop = 656
-thresh_heat = 4
+thresh_heat = 1
+debug = False
+isvideo = False
 # Definition of feature hyper parameters
 Spatial_size = (32, 32)
-Hist_bins = 32
-Orient = 9
+Hist_bins = 16
+Orient = 12
 Pix_per_cell = 8
 Pix_per_window = 64
 Cell_per_block = 2
-Hog_channel = 0
+Hog_channel = 'ALL'  # type: int
+Color_space = 'HLS'
 # Definition of SVC hyper parameters
 X_scaler = None  # type: StandardScaler
 svc_model = None  # type: SVC
-epochs = 3  # Number of epoch in training
+epochs = 2  # Number of epoch in training
 valid_port = 0.2  # Test size in proportion
-C_SVC = 1.0  # Paramtere C in SVC
-gamma_SVC = 1.2  # Parameter gamma in SVC
+C_SVC = 1.1  # Paramtere C in SVC
+gamma_SVC = 0.01  # Parameter gamma in SVC
+train_sample = 0
+tuning = False
+parameters = {'gamma': [0.01, 0.1, 1], 'C': [1, 10], 'kernel': ('linear', 'rbf')}
 
 
-def find_cars(img, ystart, ystop, scale, svc, x_scaler, color_space='RGB'):
-    img = img.astype(np.float32) / 255
-
-    img_tosearch = img[ystart:ystop, :, :]
+def find_cars(img, xzone, yzone, scale, svc, x_scaler):
+    img_tosearch = img[yzone[0]:yzone[1], xzone[0]:xzone[1], :]
     ctrans_tosearch = np.copy(img_tosearch)
-    if color_space != 'RGB':
-        if color_space == 'HSV':
+    if Color_space != 'RGB':
+        if Color_space == 'HSV':
             ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
+        elif Color_space == 'LUV':
             ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
+        elif Color_space == 'HLS':
             ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
+        elif Color_space == 'YUV':
             ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
+        elif Color_space == 'YCrCb':
             ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YCrCb)
     if scale != 1:
         imshape = ctrans_tosearch.shape
@@ -72,9 +77,10 @@ def find_cars(img, ystart, ystop, scale, svc, x_scaler, color_space='RGB'):
     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
     window = Pix_per_window
     nblocks_per_window = (window // Pix_per_cell) - Cell_per_block + 1
-    cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step + 1
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step + 1
+    cells_per_step_x = 1  # Instead of overlap, define how many cells to step
+    cells_per_step_y = 1  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step_x + 1
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step_y + 1
 
     # Compute individual channel HOG features for the entire image
     hog_array = [get_hog_features(ch1, Orient, Pix_per_cell, Cell_per_block, feature_vec=False),
@@ -85,8 +91,8 @@ def find_cars(img, ystart, ystop, scale, svc, x_scaler, color_space='RGB'):
 
     for xb in range(nxsteps):
         for yb in range(nysteps):
-            ypos = yb * cells_per_step
-            xpos = xb * cells_per_step
+            ypos = yb * cells_per_step_y
+            xpos = xb * cells_per_step_x
             # Extract HOG for this patch
             if Hog_channel == 'ALL':
                 hog_feat1 = hog_array[0][ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
@@ -114,29 +120,29 @@ def find_cars(img, ystart, ystop, scale, svc, x_scaler, color_space='RGB'):
             test_prediction = svc.predict(test_features)
 
             if test_prediction == 1:
-                xbox_left = np.int(xleft * scale)
-                ytop_draw = np.int(ytop * scale + ystart)
+                xbox_left = np.int(xleft * scale + xzone[0])
+                ytop_draw = np.int(ytop * scale + yzone[0])
                 win_draw = np.int(window * scale)
                 box_list.append(((xbox_left, ytop_draw), (xbox_left + win_draw, ytop_draw + win_draw)))
 
     return box_list
 
 
-def single_img_features(img, color_space='RGB', spatial_feat=True, hist_feat=True, hog_feat=True):
+def single_img_features(img, spatial_feat=True, hist_feat=True, hog_feat=True):
     # 1) Define an empty list to receive features
     img_features = []
     feature_image = []
     # 2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
+    if Color_space != 'RGB':
+        if Color_space == 'HSV':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
+        elif Color_space == 'LUV':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
+        elif Color_space == 'HLS':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
+        elif Color_space == 'YUV':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
+        elif Color_space == 'YCrCb':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
     else:
         feature_image = np.copy(img)
@@ -168,8 +174,6 @@ def single_img_features(img, color_space='RGB', spatial_feat=True, hist_feat=Tru
 
 
 def training(input_file):
-    data = []
-    y_data = []
     files = []
     paths = os.listdir(input_file + '\\vehicles')
     for path in paths:
@@ -177,63 +181,96 @@ def training(input_file):
             pngs = os.listdir(input_file + '\\vehicles\\' + path)
             for png in pngs:
                 if 'png' in png or 'jpg' in png:
-                    files.append(input_file + '\\vehicles\\' + path + '\\' + png)
-                    y_data.append(1)
+                    files.append([input_file + '\\vehicles\\' + path + '\\' + png, 1])
+    num_veh = len(files)
     paths = os.listdir(input_file + '\\non-vehicles')
     for path in paths:
         if '.' not in path:
             pngs = os.listdir(input_file + '\\non-vehicles\\' + path)
             for png in pngs:
                 if 'png' in png or 'jpg' in png:
-                    files.append(input_file + '\\non-vehicles\\' + path + '\\' + png)
-                    y_data.append(0)
+                    files.append([input_file + '\\non-vehicles\\' + path + '\\' + png, 0])
+    num_non_veh = len(files) - num_veh
+    if 0 < train_sample / 2 < min(num_veh, num_non_veh):
+        ind_veh = np.linspace(0, num_veh - 1, num_veh, dtype=np.int)
+        ind_non_veh = np.linspace(0, num_non_veh - 1, num_non_veh, dtype=np.int) + num_veh
+        ind_veh = sklearn.utils.shuffle(ind_veh)[0:int(train_sample / 2)]
+        ind_non_veh = sklearn.utils.shuffle(ind_non_veh)[0:int(train_sample / 2)]
+        ind_files = np.concatenate([ind_veh, ind_non_veh])
+    else:
+        ind_files = np.linspace(0, num_veh + num_non_veh - 1, num_veh + num_non_veh, dtype=np.int)
 
-    for file in files:
-        img = cv2.imread(file)
-        feature = single_img_features(img, color_space='YCrCb', spatial_feat=True, hist_feat=True, hog_feat=True)
-        data.append(feature)
+    feature_ini = single_img_features(cv2.imread(files[0][0]), spatial_feat=True,
+                                      hist_feat=True, hog_feat=True)
+    data = [feature_ini] * len(ind_files)
+    labels = [1] * len(ind_files)
+    for i in range(len(ind_files)):
+        ind = ind_files[i]
+        img = cv2.imread(files[ind][0])
+        feature = single_img_features(img, spatial_feat=True, hist_feat=True, hog_feat=True)
+        data[i] = feature
+        labels[i] = files[ind][1]
 
-    svc = SVC(C=C_SVC, kernel='linear', gamma=gamma_SVC)
     X_data = np.vstack(data).astype(np.float64)
     scaler = StandardScaler().fit(X_data)
     X_data_scale = scaler.transform(X_data)
     accuracy = []
-    X_data_scale, y_data = sklearn.utils.shuffle(X_data_scale, y_data)
-    for epoch in range(epochs):
-        X_train, X_valid, y_train, y_valid = train_test_split(X_data_scale, y_data, test_size=valid_port, shuffle=True)
-        svc.fit(X_train, y_train)
-        accuracy.append(svc.score(X_valid, y_valid))
-        print('EPOCH {}: Accuracy is {:.3f}'.format(epoch, accuracy[epoch] * 100))
+    X_data_scale, labels = sklearn.utils.shuffle(X_data_scale, labels)
+    res = {'best_par': None, 'model': None, 'scaler': None}
+    if tuning:
+        svc = SVC()
+        clf = GridSearchCV(svc, parameters)
+        clf.fit(X_data_scale, labels)
+        res['best_par'] = clf.best_params_
+    else:
+        svc = SVC(C=C_SVC, kernel='linear', gamma=gamma_SVC)
+        for epoch in range(epochs):
+            X_train, X_valid, y_train, y_valid = train_test_split(X_data_scale, labels,
+                                                                  test_size=valid_port, shuffle=True)
+            svc.fit(X_train, y_train)
+            accuracy.append([svc.score(X_train, y_train), svc.score(X_valid, y_valid)])
+            print('EPOCH {}: Training accuracy is {:.3f}; validation accuracy is {:.3f}'.
+                  format(epoch, accuracy[epoch][0] * 100, accuracy[epoch][1] * 100))
 
-    return svc, scaler
+    res['model'] = svc
+    res['scaler'] = scaler
+    return res
 
 
 def pipeline(image):
-    scales = np.array([1, 1.25, 1.75, 2, 3, 4])
+    if isvideo:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    scales = np.array([1, 1.25, 1.5, 2, 3, 4])
     y_start = 400
-    ystop = np.int_(y_start + scales * Pix_per_window)
     bbox_list = []
     heat = np.zeros_like(image[:, :, 0]).astype(np.float)
     for i in range(len(scales)):
-        bbox_list += find_cars(image, y_start, ystop[i], scales[i], svc_model, X_scaler, color_space='YCrCb')
+        y_stop = np.int_(y_start + scales[i] * Pix_per_window)
+        x_start = 0
+        x_stop = image.shape[1]
+        bbox_list += find_cars(image, (x_start, x_stop), (y_start, y_stop), scales[i], svc_model, X_scaler)
 
-    draw_img = np.copy(image)  # Debug
-    for box in bbox_list:  # Debug
-        cv2.rectangle(draw_img, box[0], box[1], (0, 0, 255), 6)  # Debug
-    cv2.imwrite('output_images/debug/box.jpg', draw_img)  # Debug
     heat = add_heat(heat, bbox_list)
     heat = apply_threshold(heat, thresh_heat)
     heatmap = np.clip(heat, 0, 255)
-    cv2.imwrite('output_images/debug/heatmap.jpg', heatmap)  # Debug
+    if debug:
+        debug_img = np.copy(image)  # Debug
+        for box in bbox_list:  # Debug
+            cv2.rectangle(debug_img, box[0], box[1], (0, 0, 255), 6)  # Debug
+        cv2.imwrite('output_images/debug/box.jpg', debug_img)  # Debug
+        cv2.imwrite('output_images/debug/heatmap.jpg', heatmap / np.max(heatmap) * 255)  # Debug
     labels = label(heatmap)
     draw_img = draw_labeled_bboxes(np.copy(image), labels)
+    if isvideo:
+        draw_img = cv2.cvtColor(draw_img, cv2.COLOR_RGB2BGR)
 
     return draw_img
 
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "tpi:o:m:", ["training", "prediction", "input=", "output=", "Model="])
+        opts, args = getopt.getopt(sys.argv[1:], "tpudi:o:m:s:", ["training", "prediction", "tuning", "debug",
+                                                                  "input=", "output=", "model=", "size="])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(-1)
@@ -242,7 +279,7 @@ def main():
     input_file = ''
     output_file = ''
     model_file = ''
-    global svc_model, X_scaler
+    global svc_model, X_scaler, train_sample, tuning, debug, isvideo
 
     for o, a in opts:
         if o in ('-t', '--training'):
@@ -251,12 +288,18 @@ def main():
         elif o in ('-p', '--prediction'):
             train = False
             predict = True
+        elif o in ('-u', '--tuning'):
+            tuning = True
+        elif o in ('-d', '--debug'):
+            debug = True
         elif o in ('-i', '--input'):
             input_file = a
         elif o in ('-o', '--output'):
             output_file = a
         elif o in ('-m', '--model'):
             model_file = a
+        elif o in ('-s', '--size') and a.isdecimal():
+            train_sample = int(a)
         else:
             assert False, 'Unhandled option'
 
@@ -268,13 +311,12 @@ def main():
 
     if train:
         print('Training mode. Training data folder: ' + input_file)
-        svc, X_scaler = training(input_file)
-        svc_dict = {'model': svc, 'scaler': X_scaler}
+        svc_dict = training(input_file)
         pickle.dump(svc_dict, open(output_file, 'wb'))
         print('Model save to ' + output_file)
 
     if predict:
-        print('Prediction mode' + output_file)
+        print('Prediction mode. Input file: ' + input_file)
         if model_file == '':
             files = os.listdir('.\\')
             i = 0
@@ -297,7 +339,8 @@ def main():
                 print('Can''t find images')
                 sys.exit(-2)
         elif '.mp4' in input_file.split('\\')[-1]:
-            clip1 = VideoFileClip(input_file.split('\\')[-1]).subclip(1, 5)
+            isvideo = True
+            clip1 = VideoFileClip(input_file.split('\\')[-1])  # .subclip(26, 30)
             lane_clip = clip1.fl_image(pipeline)
             lane_clip.write_videofile(output_file, audio=False)
         else:
